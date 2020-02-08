1ec3e01e3e0ee278785ea473f2477058cd2fd0ce
Frie <fr1e@pm.me>
2020-02-08 15:55:06 +0100

    update code with "real" preprocessing

--- mapping.R
+++ mapping.R
@@ -1,49 +1,30 @@
 # load packages
-
-packages <- c("readr", "tidyverse", "leaflet", "sf")
-
-for (p in packages) {
-  if (p %in% installed.packages()[,1]) {
-    print(paste0(p, ' is installed. Will now load ', p,'.'))
-    library(p, character.only=T)
-  }
-  else {
-    print(paste0(p, ' is NOT installed. Will now install ', p,'.'))
-    install.packages(p)
-    library(p, character.only=T)
-  }
-}
-
-rm(packages, p)
-
+library(readr)
+library(tidyverse)
+library(leaflet)
+library(sf)
+library(snakecase)
 # load data
 
 ## German cities with lon/lat (source: https://simplemaps.com/data/de-cities)
-de <- read_csv("data/de.csv")
 
-## dataset with subscribers from mailchimp (only available for admin)
-Mailchimp <- read_csv("data/Test.csv", col_names = FALSE)
+ca <- read_csv("data/subscribed_members_export_de459417e6.csv")
+colnames(ca) <- snakecase::to_snake_case(colnames(ca))
 
+colnames(ca)
 
-# Preparation
-
-## changing variable name for location to create a unique identifier
-Mailchimp <- Mailchimp %>%
-  rename(city = "In welcher Stadt lebst du? / In what city? *")
-
-## matching Mailchimp-df and cities' location by "city"
-df <- Mailchimp %>%
-  left_join(de, by = "city")
-
+# filter out rows with missing lat / long
+ca_sub <- ca %>% 
+  rename(city = in_welcher_stadt_lebst_du_in_what_city) %>% 
+  mutate(longitude = as.numeric(longitude),
+         latitude = as.numeric(latitude)) %>% 
+  filter(!is.na(longitude) & !is.na(latitude))
 
 ## Converting data frame to sf-Format (geometry list)
-
-df <- st_as_sf(df, coords = c("lng", "lat"), 
-               crs = 4326, agr = "constant") # sind die Angaben über crs und agr überhaupt notwendig?
+ca_aggr <- st_as_sf(ca_sub, coords = c("longitude", "latitude"))
 
 # Plotting with leaflet (clustering)
-
-leaflet(df) %>%
+leaflet(ca_aggr) %>%
   addTiles() %>%
   addMarkers(clusterOptions = markerClusterOptions(),
              label = ~as.character(city))
